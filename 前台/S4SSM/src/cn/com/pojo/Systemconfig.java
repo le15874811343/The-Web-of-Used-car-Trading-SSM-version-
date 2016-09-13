b323043d5ae36bf347d7cfc6429e9fe6fdece674
le15874811343 <576169235@qq.com>
2016-09-14 00:28:34 +0800

    车辆系统配置类

--- 前台/S4SSM/src/cn/com/pojo/Systemconfig.java
+++ 前台/S4SSM/src/cn/com/pojo/Systemconfig.java
@@ -1,43 +1,46 @@
 package cn.com.pojo;
 
 import java.io.Serializable;
-
+/**
+ * 车辆系统配置类
+ * @author lej
+ */
 public class Systemconfig implements Serializable {
-    private Long uId;
+    private Long uId;  //车主编号 
 
-    private Long cId;
+    private Long cId; //车辆编号
 
-    private String guidancesystem;
+    private String guidancesystem;  //导航系统
 
-    private String alb;
+    private String alb;  //防抱死制动系统
 
-    private String fpg;
+    private String fpg; //前驻车雷达
 
-    private String rpg;
+    private String rpg; //后驻车雷达
 
-    private String rcpa;
+    private String rcpa;  //倒车影像系统
+ 
+    private String dsea; //驾驶员座椅电动调节
 
-    private String dsea;
+    private String fsea;  //副驾驶座椅电动调节
 
-    private String fsea;
+    private String dlcc; //定速巡航
 
-    private String dlcc;
+    private String hfs; //前排座椅加热
+ 
+    private String hrs; //后排座椅加热
 
-    private String hfs;
+    private String fsv; //前排座椅通风
 
-    private String hrs;
+    private String rsv; //后排座椅通风
 
-    private String fsv;
+    private String fsm;  //前排座椅记忆
 
-    private String rsv;
+    private String rsm; //后排座椅记忆
+ 
+    private String rvmh; //后视镜加热
 
-    private String fsm;
-
-    private String rsm;
-
-    private String rvmh;
-
-    private String ess;
+    private String ess; //发动机启停
 
     public Long getuId() {
         return uId;
