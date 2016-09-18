f8592f13acd40538ae59f8ca1a9210abc8d5d806
le15874811343 <576169235@qq.com>
2016-09-18 13:47:29 +0800

    价格区间服务接口

--- 前台/S4SSM/src/cn/com/service/IPriceIntervalService.java
+++ 前台/S4SSM/src/cn/com/service/IPriceIntervalService.java
@@ -6,7 +6,16 @@ import org.springframework.stereotype.Repository;
 
 
 import cn.com.pojo.*;
+/**
+ * 价格区间服务接口
+ * @author lej
+ */
 @Repository
 public interface IPriceIntervalService {
+  /**
+   * 
+   * 按热度获取价格区间信息的服务
+   * @return Map<Integer,PriceInterval>
+   */
 	public Map<Integer, Priceinterval> getPriceIntervalByCount();
 }
