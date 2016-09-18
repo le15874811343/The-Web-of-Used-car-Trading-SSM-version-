9be21f6b9897f072b71c70b6908fda6288dd2fbc
le15874811343 <576169235@qq.com>
2016-09-18 13:52:21 +0800

    用户信息服务接口

--- 前台/S4SSM/src/cn/com/service/IUserInfoService.java
+++ 前台/S4SSM/src/cn/com/service/IUserInfoService.java
@@ -6,12 +6,35 @@ import org.springframework.stereotype.Repository;
 import cn.com.pojo.*;
 
 
-
+/**
+ * 用户信息服务接口
+ * @author
+ */
 @Repository
 public interface IUserInfoService {
+  /**
+   * 登录的服务
+   * @return UserInfo
+   */
 	public Userinfo3 login(Userinfo3 userInfo);
+  /**
+   * 按唯一条件获取用户信息的服务
+   *@return UserInfo 
+   */
 	public Userinfo3 getUserInfoByUnique(Userinfo3 userInfo);
+  /**
+   * 添加用户信息的服务
+   *@return boolean 
+   */
 	public boolean addUserInfo(Userinfo3 userInfo);
+  /**
+   * 修改用户信息的服务
+   *@return boolean 
+   */
 	public boolean updateUserInfo(Userinfo3 userInfo);
+  /**
+   * 修改用户密码的服务
+   *@return boolean 
+   */
 	public boolean updateUserPwd(Userinfo3 userInfo);
 }
