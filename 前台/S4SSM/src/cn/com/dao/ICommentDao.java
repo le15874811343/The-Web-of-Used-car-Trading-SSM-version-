1f16a9926ae3c86c06a9d537879af15cd23aefb1
le15874811343 <576169235@qq.com>
2016-09-18 13:10:59 +0800

    评论操作接口

--- 前台/S4SSM/src/cn/com/dao/ICommentDao.java
+++ 前台/S4SSM/src/cn/com/dao/ICommentDao.java
@@ -5,11 +5,37 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 评论操作接口
+ * @author lej
+ */
 @Repository
 public interface ICommentDao {
+            /**
+           * 添加评论的方法
+           * @parma comment
+           * @return int
+           */
           public int addComment(Comment1 comment);
+           /**
+           * 删除评论的方法
+           * @parma comment
+           * @return int
+           */
           public int deleteComment(Comment1 comment);
+            /**
+           * 获取评论的方法
+           * @parma comment
+           * @return Comment1
+           */
           public Comment1 getComment(Comment1 comment);
+            /**
+           * 获取两条最新的评论的方法
+           * @parma comment
+           * @parma min 最小行
+           * @parma max 最大行
+           * @return Map<Long,Comment1>
+           */
           public Map<Long, Comment1> getTheTowComment(Comment1  comment,int min,int max);
         
 }
