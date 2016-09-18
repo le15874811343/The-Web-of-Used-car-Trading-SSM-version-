1ae2b86cc6001a075de32de657cff5c3cc2999e4
le15874811343 <576169235@qq.com>
2016-09-18 20:07:47 +0800

    价格区间操作实现类

--- 前台/S4SSM/src/cn/com/dao/impl/PriceIntervalDaoImpl.java
+++ 前台/S4SSM/src/cn/com/dao/impl/PriceIntervalDaoImpl.java
@@ -7,10 +7,17 @@ import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
 import cn.com.dao.*;
-
+/**
+ * 价格区间操作实现类
+ * @author lej
+ */
 @Repository
 public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao{
-
+  /**
+   * 
+   * 按热度获取价格区间信息的方法
+   * @return Map<Integer,PriceInterval>
+   */
 	@Override
 	public Map<Integer, Priceinterval> getPriceIntervalByCount() {
 		// TODO Auto-generated method stub
