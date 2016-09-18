0d3029422e2cb11a7a8702ef052191caec8b9a16
le15874811343 <576169235@qq.com>
2016-09-18 13:11:27 +0800

    行驶距离操作接口

--- 前台/S4SSM/src/cn/com/dao/IDistanceDao.java
+++ 前台/S4SSM/src/cn/com/dao/IDistanceDao.java
@@ -4,7 +4,16 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 行驶距离操作接口
+ *@author lej 
+ */
 @Repository
 public interface IDistanceDao {
+   /**
+    * 
+    * 按热度获取行驶距离信息的方法
+    *@return Map<Integer,Distance> 
+    */
    public Map<Integer, Distance> getDistanceByCount();
 }
