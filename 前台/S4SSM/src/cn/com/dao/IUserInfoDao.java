14a7809bfd699e7bc7337f7a10700e9731a19fa7
le15874811343 <576169235@qq.com>
2016-09-18 13:23:49 +0800

    用户信息操作接口

--- 前台/S4SSM/src/cn/com/dao/IUserInfoDao.java
+++ 前台/S4SSM/src/cn/com/dao/IUserInfoDao.java
@@ -4,11 +4,35 @@ package cn.com.dao;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 用户信息操作接口
+ * @author
+ */
 @Repository
 public interface IUserInfoDao {
+   /**
+   * 登录的方法
+   * @return Userinfo3
+   */
   public Userinfo3 login(Userinfo3 userInfo);
+   /**
+   * 按唯一条件获取用户信息的方法
+   *@return Userinfo3 
+   */
   public Userinfo3 getUserInfoByUnique(Userinfo3 userInfo);
+   /**
+   * 添加用户信息的方法
+   *@return int 
+   */
   public int addUserInfo(Userinfo3 userInfo);
+  /**
+   * 修改用户信息的方法
+   *@return int 
+   */
   public int updateUserInfo(Userinfo3 userInfo);
+  /**
+   * 修改用户密码的方法
+   *@return int 
+   */
   public int updateUserPwd(Userinfo3 userInfo);
 }
