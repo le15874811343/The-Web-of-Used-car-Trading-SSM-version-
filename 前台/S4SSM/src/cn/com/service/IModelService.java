3adf050fe91f955c28d8ea925c94a52036931308
le15874811343 <576169235@qq.com>
2016-09-18 13:44:51 +0800

    车系信息服务接口

--- 前台/S4SSM/src/cn/com/service/IModelService.java
+++ 前台/S4SSM/src/cn/com/service/IModelService.java
@@ -5,7 +5,16 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 车系信息服务接口
+ * @author lej
+ */
 @Repository
 public interface IModelService {
+      /**
+       * 按条件获取车系信息的服务
+       * @parma model
+       *@return List<Model> 
+       */
 	public List<Model> getModelByWhere(Model model);
 }
