834d8eac1446873e72b5d9adb7fcbdec88ac4491
le15874811343 <576169235@qq.com>
2016-09-18 13:05:47 +0800

    品牌信息操作接口

--- 前台/S4SSM/src/cn/com/dao/ICarBrandDao.java
+++ 前台/S4SSM/src/cn/com/dao/ICarBrandDao.java
@@ -4,10 +4,31 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 品牌信息操作接口
+ * @author lej
+ */
 @Repository
 public interface ICarBrandDao {
+   /**
+   * 按热度获取品牌信息的方法
+   * @return  Map<Integer, Carbrand>
+   */
   public Map<Integer, Carbrand> getCarBrandByCount();
+   /**
+   * 获取所有品牌信息的方法
+   * @return Map<Integer,Carbrand>
+   */
   public Map<Integer, Carbrand> getAllBrand();
+    /**
+   * 根据品牌编号获取品牌信息的方法
+   * @param carBrand
+   * @return Carbrand
+   */
   public Carbrand getBrandByID(Carbrand carBrand);
+  /**
+   * 根据品牌名称获取品牌的方法
+   *@return Carbrand 
+   */
   public Carbrand getBrandByName(Carbrand carBrand);
 }
