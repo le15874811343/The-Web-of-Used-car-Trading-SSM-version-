840fa72fc7628316553c445cbd91c3769c6d1563
le15874811343 <576169235@qq.com>
2016-09-18 13:12:35 +0800

    汽车硬件配置信息操作接口

--- 前台/S4SSM/src/cn/com/dao/IHardwareConfig.java
+++ 前台/S4SSM/src/cn/com/dao/IHardwareConfig.java
@@ -5,10 +5,34 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 汽车硬件配置信息操作接口
+ *@author lej 
+ */
 @Repository
 public interface IHardwareConfig {
+  /**
+   * 
+   * 获取所有汽车硬件配置信息的方法
+   *@return Map<Long,Hardwareconfig> 
+   */
 public Map<Long, Hardwareconfig> getAllHardwareConfig();
+/**
+ * 按编号获取汽车硬件配置信息的方法
+ * @parma carInfo
+ * @return Hardwareconfig 
+ */
 public Hardwareconfig getHardwareConfigById(Carinfo carInfo);
+/**
+ * 添加汽车硬件配置信息的方法
+ * @parma hardwareConfig
+ * @return int
+ */
 public int addHardwareConfig(Hardwareconfig hardwareConfig);
+/**
+ * 修改汽车硬件配置信息的方法
+ * @parma hardwareConfig
+ * @return int
+ */
 public int updateHardwareConfig(Hardwareconfig hardwareConfig);
 }
