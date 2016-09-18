8c8f44f545526a1b58d6b8e2056c07365fef3f68
le15874811343 <576169235@qq.com>
2016-09-18 14:06:40 +0800

    行驶距离服务实现类

--- 前台/S4SSM/src/cn/com/service/impl/DistanceServiceImpl.java
+++ 前台/S4SSM/src/cn/com/service/impl/DistanceServiceImpl.java
@@ -9,16 +9,25 @@ import org.springframework.stereotype.Service;
 import cn.com.pojo.*;
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 行驶距离服务实现类
+ *@author lej 
+ */
 @Service
 public class DistanceServiceImpl implements IDistanceService {
 	@Resource
-   private IDistanceDao distanceDao=null;
+   private IDistanceDao distanceDao=null; //行驶距离操作接口的引用
 	public IDistanceDao getDistanceDao() {
 	return distanceDao;
 }
 public void setDistanceDao(IDistanceDao distanceDao) {
 	this.distanceDao = distanceDao;
 }
+   /**
+    * 
+    * 按热度获取行驶距离信息的服务
+    *@return Map<Integer,Distance> 
+    */
 	@Override
 	public Map<Integer, Distance> getDistanceByCount() {
 		// TODO Auto-generated method stub
