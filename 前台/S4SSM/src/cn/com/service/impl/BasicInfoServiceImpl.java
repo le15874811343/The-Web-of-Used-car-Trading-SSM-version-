7b72baa4acd76b7a59b2cc1946145fd5ef053069
le15874811343 <576169235@qq.com>
2016-09-18 13:57:11 +0800

    汽车基本信息服务实现类

--- 前台/S4SSM/src/cn/com/service/impl/BasicInfoServiceImpl.java
+++ 前台/S4SSM/src/cn/com/service/impl/BasicInfoServiceImpl.java
@@ -11,10 +11,14 @@ import cn.com.pojo.*;
 import cn.com.dao.IBasicInfoDao;
 
 import cn.com.service.*;
+/**
+ * 汽车基本信息服务实现类
+ * @author lej
+ */
 @Service
 public class BasicInfoServiceImpl implements IBasicInfoService{
 	@Resource
-private IBasicInfoDao basicInfoDaoImpl=null;
+private IBasicInfoDao basicInfoDaoImpl=null; //汽车基本信息操作接口的引用
 
 public IBasicInfoDao getBasicInfoDaoImpl() {
 	return basicInfoDaoImpl;
@@ -23,19 +27,28 @@ public IBasicInfoDao getBasicInfoDaoImpl() {
 public void setBasicInfoDaoImpl(IBasicInfoDao basicInfoDaoImpl) {
 	this.basicInfoDaoImpl = basicInfoDaoImpl;
 }
-
+/**
+ * 获取所有基本信息的服务
+ * @return Map<Long, BasicInfo>
+*/
 @Override
 public Map<Long, Basicinfo> getAllBasic() {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.getAllBasic();
 }
-
+/**
+ *根据编号获取汽车基本信息的服务
+ *@return BasicInfo
+ */
 @Override
 public Basicinfo getAllBasicById(Carinfo carInfo) {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.getAllBasicById(carInfo);
 }
-
+/**
+ * 添加汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean addBasicInfo(Basicinfo basicInfo) {
 	// TODO Auto-generated method stub
@@ -45,7 +58,10 @@ public boolean addBasicInfo(Basicinfo basicInfo) {
 	}
 	return flag;
 }
-
+/**
+ * 修改汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean updateBasicInfo(Basicinfo basicInfo) {
 	// TODO Auto-generated method stub
