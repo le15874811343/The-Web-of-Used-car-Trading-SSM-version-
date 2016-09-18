232a6573b37ada8ed013f56dc91d9256d809db46
le15874811343 <576169235@qq.com>
2016-09-18 19:59:00 +0800

    排放标准操作实现类

--- 前台/S4SSM/src/cn/com/dao/impl/EmissionstandardDaoImpl.java
+++ 前台/S4SSM/src/cn/com/dao/impl/EmissionstandardDaoImpl.java
@@ -6,9 +6,17 @@ import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
 import cn.com.dao.*;
+/**
+ * 
+ * 排放标准操作实现类
+ *@author  lej
+ */
 @Repository
 public class EmissionstandardDaoImpl extends BaseDao implements IEmissionstandardDao {
-
+        /**
+	 * 按热度获取排放标准信息的方法
+	 * @Map<Integer,Emissionstandard>
+	 */
 	@Override
 	public Map<Integer, Emissionstandard> getEmissionstandardByCount() {
 		// TODO Auto-generated method stub
