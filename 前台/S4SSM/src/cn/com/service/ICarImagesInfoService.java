401188c9be683e7e6cffb78c9c24c7a3a182794e
le15874811343 <576169235@qq.com>
2016-09-18 13:27:09 +0800

    汽车图片信息服务接口

--- 前台/S4SSM/src/cn/com/service/ICarImagesInfoService.java
+++ 前台/S4SSM/src/cn/com/service/ICarImagesInfoService.java
@@ -5,10 +5,29 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 汽车图片信息服务接口
+ * @author lej
+ */
 @Repository
 public interface ICarImagesInfoService {
+  /**
+   * 根据编号获取汽车照片的服务
+   * @parma carInfo
+   * @return Map<Integer,String>
+   */
 	public Map<Integer, String> getCarImagesInfoByID(Carinfo carInfo);
+/**
+ * 添加照片信息的服务
+ * @parma carImagesInfo
+ *@reutn boolean 
+ */
 	public boolean addCarImagesInfo(Imagesinfo carImagesInfo);
+/**
+ *修改照片信息的服务
+ * @parmas
+ * @return boolean
+ */
 	public boolean updateCarImagesInfo(Imagesinfo carImagesInfo);
 	
 }
