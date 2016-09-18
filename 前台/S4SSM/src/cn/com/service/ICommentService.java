bd04a4bb939c051c2d793a912e6e262432b6594c
le15874811343 <576169235@qq.com>
2016-09-18 13:42:36 +0800

    评论服务接口

--- 前台/S4SSM/src/cn/com/service/ICommentService.java
+++ 前台/S4SSM/src/cn/com/service/ICommentService.java
@@ -5,10 +5,36 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 评论服务接口
+ * @author lej
+ */
 @Repository
 public interface ICommentService {
+	  /**
+           * 添加评论的服务
+           * @parma comment
+           * @return boolean
+           */
 	public boolean addComment(Comment1 comment) ;
+	  /**
+           * 删除评论的服务
+           * @parma comment
+           * @return boolean
+           */
 	public boolean deleteComment(Comment1 comment);
+          /**
+           * 获取评论的服务
+           * @parma comment
+           * @return Comment1
+           */
 	public Comment1 getComment(Comment1 comment);
+	  /**
+           * 获取两条最新的评论的服务
+           * @parma comment
+           * @parma min 最小行
+           * @parma max 最大行
+           * @return Map<Long,Comment1>
+           */
 	public Map<Long, Comment1> getTheTowComment(Comment1 comment,int min,int max);
 }
