8f1ad87bca765b3c16a3cd9c752090e778cde8f3
le15874811343 <576169235@qq.com>
2016-09-18 13:10:24 +0800

    车型操作接口

--- 前台/S4SSM/src/cn/com/dao/ICarTypeDao.java
+++ 前台/S4SSM/src/cn/com/dao/ICarTypeDao.java
@@ -5,8 +5,20 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 车型操作接口
+ * @author
+ */
 @Repository
 public interface ICarTypeDao {
+	/**
+	 * 按热度获取车型信息的方法
+	 * @return Map<Integer,Cartype>
+	 */
 	public Map<Integer, Cartype> getCarTypeByCount();
+	/**
+	 * 获取所有车型信息的方法
+	 * @return  Map<Integer,Cartype>
+	 */
 	public Map<Integer, Cartype> getAllCarType();
 }
