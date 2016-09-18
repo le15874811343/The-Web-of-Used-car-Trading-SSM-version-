a95ab3fe35d9d35e51752b51641ca2148faa7543
le15874811343 <576169235@qq.com>
2016-09-18 13:25:22 +0800

    车龄信息服务接口

--- 前台/S4SSM/src/cn/com/service/ICarAgeService.java
+++ 前台/S4SSM/src/cn/com/service/ICarAgeService.java
@@ -4,7 +4,15 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 车龄信息服务接口
+ *@author  lej 
+ */
 @Repository
 public interface ICarAgeService {
+	/**
+	 * 按热度获取车龄信息的服务
+	 * @return Map<Integer,CarAge>
+	 */
 	public Map<Integer,Carage> getCarAgeByCount();
 }
