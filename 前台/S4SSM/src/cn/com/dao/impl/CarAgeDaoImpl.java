64e9bb962e205ab66109ba4049f692e5765cfd96
le15874811343 <576169235@qq.com>
2016-09-18 19:24:04 +0800

    车龄信息操作实现类

--- 前台/S4SSM/src/cn/com/dao/impl/CarAgeDaoImpl.java
+++ 前台/S4SSM/src/cn/com/dao/impl/CarAgeDaoImpl.java
@@ -5,14 +5,21 @@ import cn.com.dao.*;
 import java.util.*;
 
 import org.springframework.stereotype.Repository;
+/**
+ * 车龄信息操作实现类
+ *@author  lej 
+ */
 @Repository
 public class CarAgeDaoImpl extends BaseDao implements ICarAgeDao{
-  
+        /**
+	 * 按热度获取车龄信息的方法
+	 * @return Map<Integer,Carage>
+	 */
 	@Override
 	public Map<Integer, Carage> getCarAgeByCount() {
 		// TODO Auto-generated method stub
 		Map<Integer, Carage> carAgeMap=new HashMap<Integer, Carage>();
-	
+	//获取结果集
 	  carAgeMap=super.getSqlSessionTemplate().selectMap("cn.com.pojo.CarageMapper.getCarAgeByCount",  "aId");
 	
 		return carAgeMap;
