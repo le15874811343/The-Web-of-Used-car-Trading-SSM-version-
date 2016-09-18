ab50ee037269b6c81d8da76bf7817f8a0d0213a9
le15874811343 <576169235@qq.com>
2016-09-18 13:43:06 +0800

    行驶距离服务接口

--- 前台/S4SSM/src/cn/com/service/IDistanceService.java
+++ 前台/S4SSM/src/cn/com/service/IDistanceService.java
@@ -5,7 +5,16 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 行驶距离服务接口
+ *@author lej 
+ */
 @Repository
 public interface IDistanceService {
+   /**
+    * 
+    * 按热度获取行驶距离信息的服务
+    *@return Map<Integer,Distance> 
+    */
 	public Map<Integer, Distance> getDistanceByCount();
 }
