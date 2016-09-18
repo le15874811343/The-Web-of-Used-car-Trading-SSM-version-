f8a3145fd63a48cf585251a1f7f2987a34a3920f
le15874811343 <576169235@qq.com>
2016-09-18 14:08:48 +0800

    车系信息服务实现类

--- 前台/S4SSM/src/cn/com/service/impl/ModelServiceImpl.java
+++ 前台/S4SSM/src/cn/com/service/impl/ModelServiceImpl.java
@@ -11,10 +11,14 @@ import cn.com.pojo.*;
 
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 车系信息服务实现类
+ * @author lej
+ */
 @Service
 public class ModelServiceImpl implements IModelService{
 	@Resource
-  private IModelDao modelDao=null;
+  private IModelDao modelDao=null; //车系操作接口的引用
 
 public IModelDao getModelDao() {
 	return modelDao;
@@ -23,7 +27,11 @@ public IModelDao getModelDao() {
 public void setModelDao(IModelDao modelDao) {
 	this.modelDao = modelDao;
 }
-
+      /**
+       * 按条件获取车系信息的服务
+       * @parma model
+       *@return List<Model> 
+       */
 @Override
 public List<Model> getModelByWhere(Model model) {
 	// TODO Auto-generated method stub
