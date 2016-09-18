d1359cb3158fea6888902c2c8f14e2a1356aa1f0
le15874811343 <576169235@qq.com>
2016-09-18 14:31:51 +0800

    编码过滤器类

--- 前台/S4SSM/src/cn/com/filter/EncodingFilter.java
+++ 前台/S4SSM/src/cn/com/filter/EncodingFilter.java
@@ -10,7 +10,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/**
+ * 编码过滤器类
+ * @author  lej
+ */
 public class EncodingFilter implements Filter{
    private String encoding="";
 	@Override
@@ -25,15 +28,15 @@ public class EncodingFilter implements Filter{
 		// TODO Auto-generated method stub
 		      HttpServletRequest request=(HttpServletRequest) arg0;
 		      HttpServletResponse response=(HttpServletResponse) arg1;
-		      request.setCharacterEncoding(encoding);
-		      response.setCharacterEncoding(encoding);
-		      arg2.doFilter(request, response);
+		      request.setCharacterEncoding(encoding); //设置请求编码
+		      response.setCharacterEncoding(encoding); //设置响应编码
+		      arg2.doFilter(request, response); //通过
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
-	encoding=	arg0.getInitParameter("Encoding");
+	encoding=	arg0.getInitParameter("Encoding"); //获取配置文件中的编码参数
 	}
 
 }
