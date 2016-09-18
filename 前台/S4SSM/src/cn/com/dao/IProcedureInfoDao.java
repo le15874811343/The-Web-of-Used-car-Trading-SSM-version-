55b1e6b5b3c4b1554df26419112b7e8d07d9cd91
le15874811343 <576169235@qq.com>
2016-09-18 13:21:01 +0800

    手续信息操作接口

--- 前台/S4SSM/src/cn/com/dao/IProcedureInfoDao.java
+++ 前台/S4SSM/src/cn/com/dao/IProcedureInfoDao.java
@@ -5,10 +5,34 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 手续信息操作接口
+ * @author lej
+ */
 @Repository
 public interface IProcedureInfoDao {
+   /**
+   * 
+   * 获取所有手续信息的方法
+   *@return Map<Long,Procedureinfo> 
+   */
 public Map<Long, Procedureinfo> getAllProcedureInfo();
+  /**
+   * 
+   * 根据编号获取手续信息的方法
+   *@return Procedureinfo
+   */
 public Procedureinfo getProcedureInfoById(Carinfo carInfo);
+  /**
+   * 
+   * 添加手续信息的方法
+   *@return int
+   */
 public int addProcedureInfo(Procedureinfo procedureInfo);
+  /**
+   * 
+   * 修改手续信息的方法
+   *@return  int
+   */
 public int updateProcedureInfo(Procedureinfo procedureInfo);
 }
