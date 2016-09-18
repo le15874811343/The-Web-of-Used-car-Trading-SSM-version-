e6f5c2381f959c7cc5961a091eae971f507a8410
le15874811343 <576169235@qq.com>
2016-09-18 20:00:25 +0800

    车系信息操作实现类

--- 前台/S4SSM/src/cn/com/dao/impl/ModelDaoImpl.java
+++ 前台/S4SSM/src/cn/com/dao/impl/ModelDaoImpl.java
@@ -5,9 +5,17 @@ import cn.com.dao.*;
 import java.util.*;
 
 import org.springframework.stereotype.Repository;
+/**
+ * 车系信息操作实现类
+ * @author lej
+ */
 @Repository
 public class ModelDaoImpl extends BaseDao implements IModelDao {
-
+      /**
+       * 按条件获取车系信息的方法
+       * @parma model
+       *@return List<Model> 
+       */
 	@Override
 	public List<Model> getModelByWhere(Model model) {
 		// TODO Auto-generated method stub
