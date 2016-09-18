5c3ccff22fcdde5c35005493bad11c38474a3027
le15874811343 <576169235@qq.com>
2016-09-18 14:07:27 +0800

    排放标准服务实现类

--- 前台/S4SSM/src/cn/com/service/impl/EmissionstandardServiceImpl.java
+++ 前台/S4SSM/src/cn/com/service/impl/EmissionstandardServiceImpl.java
@@ -9,16 +9,25 @@ import cn.com.pojo.*;
 
 import cn.com.dao.IEmissionstandardDao;
 import cn.com.service.*;
+/**
+ * 
+ * 排放标准服务实现类
+ *@author  lej
+ */
 @Service
 public class EmissionstandardServiceImpl implements IEmissionstandardService {
 	@Resource
-    private IEmissionstandardDao emissionstandardDao=null;
+    private IEmissionstandardDao emissionstandardDao=null;//排放标准操作接口的引用
 	public IEmissionstandardDao getEmissionstandardDao() {
 		return emissionstandardDao;
 	}
 	public void setEmissionstandardDao(IEmissionstandardDao emissionstandardDao) {
 		this.emissionstandardDao = emissionstandardDao;
 	}
+	/**
+	 * 按热度获取排放标准信息的服务
+	 * @Map<Integer,Emissionstandard>
+	 */
 	@Override
 	public Map<Integer, Emissionstandard> getEmissionstandardByCount() {
 		// TODO Auto-generated method stub
