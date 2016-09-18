26e4054c914d3810074f8c1f29f19690f97e585c
le15874811343 <576169235@qq.com>
2016-09-18 13:15:22 +0800

    个人汽车订单信息操作接口

--- 前台/S4SSM/src/cn/com/dao/IPersonCarDao.java
+++ 前台/S4SSM/src/cn/com/dao/IPersonCarDao.java
@@ -4,13 +4,45 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 个人汽车订单信息操作接口
+ * @author lej
+ */
 @Repository
 public interface IPersonCarDao {
+       /**
+        * 根据条件获取用户订单信息数量
+        * @return int
+        */
        public Long getCarCountByWhere(Personcar perSonCar);
+       /**
+        * 根据条件获取用户订单信息集合
+        * @return  Map<Long, Personcar>
+        */
        public Map<Long, Personcar> getPerSonCarMapByWhere(Personcar perSonCar);
+       /**
+        *  添加用户订单信息
+        * @return int
+        */
        public int addPerSonCar(Personcar perSonCar);
+       /**
+        *  删除用户订单信息
+        * @return int
+        */
        public int deletePerSoncar(Personcar perSonCar);
+       /**
+        *  修改用户订单信息
+        * @return int
+        */
        public int updatePerSoncar(Personcar perSonCar,String state);
+       /**
+        * 根据订单状态修改订单信息的方法
+        * @return int
+        */
        public int updatePerSonCarByState(Personcar perSonCar,String state);
+       /**
+        * 获取用户订单信息的方法
+        * @return Personcar
+        */
        public Personcar getPersoncar(Personcar personcar);
 }
