5a577a8bc99b94f860da1d55160eb3a05b7553f8
le15874811343 <576169235@qq.com>
2016-09-18 13:41:47 +0800

    车型服务接口

--- 前台/S4SSM/src/cn/com/service/ICarTypeService.java
+++ 前台/S4SSM/src/cn/com/service/ICarTypeService.java
@@ -5,9 +5,20 @@ import cn.com.pojo.*;
 import java.util.*;
 
 import org.springframework.stereotype.Repository;
-
+/**
+ * 车型服务接口
+ * @author
+ */
 @Repository
 public interface ICarTypeService {
+	/**
+	 * 按热度获取车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 	public Map<Integer, Cartype> getCarTypeByCount();
+	/**
+	 * 获取所有车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 	public Map<Integer, Cartype> getAllCarType();
 }
