66858903d4920dded167414ff99d96bb1731dd2e
le15874811343 <576169235@qq.com>
2016-09-18 13:43:40 +0800

    排放标准服务接口

--- 前台/S4SSM/src/cn/com/service/IEmissionstandardService.java
+++ 前台/S4SSM/src/cn/com/service/IEmissionstandardService.java
@@ -5,7 +5,16 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 
+ * 排放标准服务接口
+ *@author  lej
+ */
 @Repository
 public interface IEmissionstandardService {
+	/**
+	 * 按热度获取排放标准信息的服务
+	 * @Map<Integer,Emissionstandard>
+	 */
 	public Map<Integer, Emissionstandard> getEmissionstandardByCount();
 }
