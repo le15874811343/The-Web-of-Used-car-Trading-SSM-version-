8b8df82efcd86cc54a640ee14d55a76eb539845b
le15874811343 <576169235@qq.com>
2016-09-18 13:47:04 +0800

    个人需求信息服务接口

--- 前台/S4SSM/src/cn/com/service/IPersonNeedService.java
+++ 前台/S4SSM/src/cn/com/service/IPersonNeedService.java
@@ -3,9 +3,28 @@ package cn.com.service;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;;
+/**
+ * 个人需求信息服务接口
+ *@author 
+ */
 @Repository
 public interface IPersonNeedService {
+  /**
+   * 
+   * 添加个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean addPersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 删除个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean deletePersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 获取个人需求信息的服务
+   *@return PersonNeed
+   */
 	public Personneed getPerSonNeed(Personneed personNeed);
 }
