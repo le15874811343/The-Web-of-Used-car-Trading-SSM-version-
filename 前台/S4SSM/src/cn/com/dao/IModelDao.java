0287e78a00702d51ba50c46243a443fd783c6d5c
le15874811343 <576169235@qq.com>
2016-09-18 13:13:12 +0800

    车系信息操作接口

--- 前台/S4SSM/src/cn/com/dao/IModelDao.java
+++ 前台/S4SSM/src/cn/com/dao/IModelDao.java
@@ -4,7 +4,16 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 车系信息操作接口
+ * @author lej
+ */
 @Repository
 public interface IModelDao {
+      /**
+       * 按条件获取车系信息的方法
+       * @parma model
+       *@return List<Model> 
+       */
       public List<Model> getModelByWhere(Model model);
 }
