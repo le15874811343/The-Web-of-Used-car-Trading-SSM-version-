3bd0e4b05c019b4719181514af2761948767ae6a
le15874811343 <576169235@qq.com>
2016-09-18 14:04:54 +0800

    车型服务实现类

--- 前台/S4SSM/src/cn/com/service/impl/CarTypeServiceImpl.java
+++ 前台/S4SSM/src/cn/com/service/impl/CarTypeServiceImpl.java
@@ -9,10 +9,14 @@ import cn.com.pojo.*;
 
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 车型服务实现类
+ * @author
+ */
 @Service
 public class CarTypeServiceImpl  implements ICarTypeService{
 	@Resource
-   private ICarTypeDao carTypeDao=null;
+   private ICarTypeDao carTypeDao=null; //车型操作接口的引用
 
 public ICarTypeDao getCarTypeDao() {
 	return carTypeDao;
@@ -21,13 +25,19 @@ public ICarTypeDao getCarTypeDao() {
 public void setCarTypeDao(ICarTypeDao carTypeDao) {
 	this.carTypeDao = carTypeDao;
 }
-
+        /**
+	 * 按热度获取车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 @Override
 public Map<Integer, Cartype> getCarTypeByCount() {
 	// TODO Auto-generated method stub
 	return carTypeDao.getCarTypeByCount();
 }
-
+        /**
+	 * 获取所有车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 @Override
 public Map<Integer, Cartype> getAllCarType() {
 	// TODO Auto-generated method stub
