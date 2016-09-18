50f61b919e6d26e76cd1c0c2f324f46bd31cbdf3
le15874811343 <576169235@qq.com>
2016-09-18 13:16:09 +0800

    个人需求信息操作接口

--- 前台/S4SSM/src/cn/com/dao/IPersonNeedDao.java
+++ 前台/S4SSM/src/cn/com/dao/IPersonNeedDao.java
@@ -3,9 +3,28 @@ package cn.com.dao;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 个人需求信息操作接口
+ *@author 
+ */
 @Repository
 public interface IPersonNeedDao {
+  /**
+   * 
+   * 添加个人需求信息的方法
+   *@return int 
+   */
 public int addPersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 删除个人需求信息的方法
+   *@return int 
+   */
 public int deletePersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 获取个人需求信息的方法
+   *@return Personneed
+   */
 public Personneed getPerSonNeed(Personneed personNeed);
 }
