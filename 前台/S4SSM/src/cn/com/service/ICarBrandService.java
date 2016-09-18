8a91d2ae996d01028457472df690aef2518fb145
le15874811343 <576169235@qq.com>
2016-09-18 13:26:21 +0800

    品牌信息服务接口

--- 前台/S4SSM/src/cn/com/service/ICarBrandService.java
+++ 前台/S4SSM/src/cn/com/service/ICarBrandService.java
@@ -4,10 +4,31 @@ import cn.com.pojo.*;
 import java.util.*;
 
 import org.springframework.stereotype.Repository;
+/**
+ * 品牌信息服务接口
+ * @author lej
+ */
 @Repository
 public interface ICarBrandService {
+  /**
+   * 按热度获取品牌信息的服务
+   * @return  Map<Integer, CarBrand>
+   */
 	public Map<Integer, Carbrand> getCarBrandByCount();
+  /**
+   * 获取所有品牌信息的服务
+   * @return Map<Integer,CarBrand>
+   */
 	public Map<Integer, Carbrand> getAllBrand();
+  /**
+   * 根据品牌编号获取品牌信息的服务
+   * @param carBrand
+   * @return CarBrand
+   */
 	public Carbrand getBrandByID(Carbrand carBrand);
+  /**
+   * 根据品牌名称获取品牌的服务
+   *@return CarBrand 
+   */
 	public Carbrand getBrandByName(Carbrand carBrand);
 }
