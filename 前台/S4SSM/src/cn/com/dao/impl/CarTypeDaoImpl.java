9e4938940c49da10ea7310a05784d3dd774b5317
le15874811343 <576169235@qq.com>
2016-09-18 19:55:43 +0800

    车型操作实现类

--- 前台/S4SSM/src/cn/com/dao/impl/CarTypeDaoImpl.java
+++ 前台/S4SSM/src/cn/com/dao/impl/CarTypeDaoImpl.java
@@ -6,10 +6,16 @@ import cn.com.dao.*;
 import java.util.*;
 
 import org.springframework.stereotype.Repository;
-
+/**
+ * 车型操作实现类
+ * @author
+ */
 @Repository
 public class CarTypeDaoImpl extends BaseDao implements ICarTypeDao {
-
+        /**
+	 * 按热度获取车型信息的方法
+	 * @return Map<Integer,CarType>
+	 */
 	@Override
 	public Map<Integer, Cartype> getCarTypeByCount() {
 		// TODO Auto-generated method stub
@@ -18,7 +24,10 @@ public class CarTypeDaoImpl extends BaseDao implements ICarTypeDao {
   carTypeMap=super.getSqlSessionTemplate().selectMap("cn.com.pojo.CartypeMapper.getCarTypeByCount", "tId");
 		return carTypeMap;
 	}
-
+        /**
+	 * 获取所有车型信息的方法
+	 * @return  Map<Integer,CarType>
+	 */
 	@Override
 	public Map<Integer, Cartype> getAllCarType() {
 		// TODO Auto-generated method stub
