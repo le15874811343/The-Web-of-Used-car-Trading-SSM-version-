f37d2115f84cc55f0a382a6a7889c22e5383534b
le15874811343 <576169235@qq.com>
2016-09-18 13:05:10 +0800

    车龄信息操作接口

--- 前台/S4SSM/src/cn/com/dao/ICarAgeDao.java
+++ 前台/S4SSM/src/cn/com/dao/ICarAgeDao.java
@@ -5,7 +5,15 @@ import org.springframework.stereotype.Repository;
 
 
 import cn.com.pojo.*;
+/**
+ * 车龄信息操作接口
+ *@author  lej 
+ */
 @Repository
 public interface ICarAgeDao {
+	/**
+	 * 按热度获取车龄信息的方法
+	 * @return Map<Integer,Carage>
+	 */
 	public Map<Integer,Carage> getCarAgeByCount();
 }
