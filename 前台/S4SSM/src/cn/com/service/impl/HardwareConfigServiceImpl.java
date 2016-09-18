d6da898480114775664345d0c4a8a09ffb99292a
le15874811343 <576169235@qq.com>
2016-09-18 14:08:15 +0800

    汽车硬件配置信息服务实现类

--- 前台/S4SSM/src/cn/com/service/impl/HardwareConfigServiceImpl.java
+++ 前台/S4SSM/src/cn/com/service/impl/HardwareConfigServiceImpl.java
@@ -9,11 +9,15 @@ import org.springframework.stereotype.Service;
 import cn.com.pojo.*;
 import cn.com.service.IHardwareConfigService;
 import cn.com.dao.IHardwareConfig;
+/**
+ * 汽车硬件配置信息服务实现类
+ *@author lej 
+ */
 @Service
 public class HardwareConfigServiceImpl implements IHardwareConfigService{
 	@Resource
-	private IHardwareConfig hardwareConfigDaoImpl=null;
-	
+	private IHardwareConfig hardwareConfigDaoImpl=null; //汽车硬件配置操作接口的引用
+	 
 	public IHardwareConfig getHardwareConfigDaoImpl() {
 		return hardwareConfigDaoImpl;
 	}
@@ -21,19 +25,31 @@ public class HardwareConfigServiceImpl implements IHardwareConfigService{
 	public void setHardwareConfigDaoImpl(IHardwareConfig hardwareConfigDaoImpl) {
 		this.hardwareConfigDaoImpl = hardwareConfigDaoImpl;
 	}
-
+ /**
+   * 
+   * 获取所有汽车硬件配置信息的服务
+   *@return Map<Long,HardwareConfig> 
+   */
 	@Override
 	public Map<Long, Hardwareconfig> getAllHardwareConfig() {
 		// TODO Auto-generated method stub
 		return hardwareConfigDaoImpl.getAllHardwareConfig();
 	}
-
+/**
+ * 按编号获取汽车硬件配置信息的服务
+ * @parma carInfo
+ * @return HardwareConfig 
+ */
 	@Override
 	public Hardwareconfig getHardwareConfigById(Carinfo carInfo) {
 		// TODO Auto-generated method stub
 		return hardwareConfigDaoImpl.getHardwareConfigById(carInfo);
 	}
-
+/**
+ * 添加汽车硬件配置信息的服务
+ * @parma hardwareConfig
+ * @return boolean
+ */
 	@Override
 	public boolean addHardwareConfig(Hardwareconfig hardwareConfig) {
 		// TODO Auto-generated method stub
@@ -43,7 +59,11 @@ public class HardwareConfigServiceImpl implements IHardwareConfigService{
 		}
 		return flag;
 	}
-
+/**
+ * 修改汽车硬件配置信息的服务
+ * @parma hardwareConfig
+ * @return boolean
+ */
 	@Override
 	public boolean updateHardwareConfig(Hardwareconfig hardwareConfig) {
 		// TODO Auto-generated method stub
