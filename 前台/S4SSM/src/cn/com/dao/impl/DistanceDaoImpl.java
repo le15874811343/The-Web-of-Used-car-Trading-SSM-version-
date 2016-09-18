04dd957381c8360eaec5797b78f65ce8ddedef9d
le15874811343 <576169235@qq.com>
2016-09-18 19:57:35 +0800

    行驶距离操作实现类

--- 前台/S4SSM/src/cn/com/dao/impl/DistanceDaoImpl.java
+++ 前台/S4SSM/src/cn/com/dao/impl/DistanceDaoImpl.java
@@ -7,10 +7,17 @@ import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
 import cn.com.dao.*;
-
+/**
+ * 行驶距离操作实现类
+ *@author lej 
+ */
 @Repository
 public class DistanceDaoImpl extends BaseDao implements IDistanceDao {
-
+   /**
+    * 
+    * 按热度获取行驶距离信息的方法
+    *@return Map<Integer,Distance> 
+    */
 	@Override
 	public Map<Integer, Distance> getDistanceByCount() {
 		// TODO Auto-generated method stub
