c01973b14dca67fc4d72103d05423ef62eda987b
le15874811343 <576169235@qq.com>
2016-09-18 14:15:59 +0800

    汽车系统配置信息服务实现类

--- 前台/S4SSM/src/cn/com/service/impl/SystemConfigServiceImpl.java
+++ 前台/S4SSM/src/cn/com/service/impl/SystemConfigServiceImpl.java
@@ -9,10 +9,16 @@ import org.springframework.stereotype.Service;
 import cn.com.pojo.*;
 import cn.com.dao.ISystemConfigDao;
 import cn.com.service.ISystemConfigService;
+/**
+ * 
+ * 
+ * 汽车系统配置信息服务实现类
+ * @author lej
+ */
 @Service
 public class SystemConfigServiceImpl implements ISystemConfigService{
 	@Resource
-	private ISystemConfigDao systemConfigDao=null;
+	private ISystemConfigDao systemConfigDao=null; //汽车系统配置信息操作接口的引用
 	
 	public ISystemConfigDao getSystemConfigDao() {
 		return systemConfigDao;
@@ -20,25 +26,30 @@ public class SystemConfigServiceImpl implements ISystemConfigService{
 	public void setSystemConfigDao(ISystemConfigDao systemConfigDao) {
 		this.systemConfigDao = systemConfigDao;
 	}
-	/**
-	 * ��ȡ����SystemConfig��ϵ�ҵ��
-	 * @return
+        /**
+	 * 获取所有系统信息
+	 * @return Map<Long, SystemConfig>
 	 */
 	@Override
 	public Map<Long, Systemconfig> getAllSystemConfig() {
 		// TODO Auto-generated method stub
 		return systemConfigDao.getAllSystemConfig();
 	}
-	/**
-	 * ͨ��u��ID��ȡSystemConfig
-	 * @param systemConfig
-	 * @return
-	 */
+/**
+ * 通过Id获取系统销售信息
+ * @param systemConfig
+ * @return carInfo
+ */
 	@Override
 	public Systemconfig getSystemConfigById(Carinfo carInfo) {
 		// TODO Auto-generated method stub
 		return systemConfigDao.getSystemConfigById(carInfo);
 	}
+/**
+ * 添加汽车系统配置信息的方法
+ * @param systemConfig
+ *@return boolean 
+ */
 	@Override
 	public boolean addSystemConfig(Systemconfig systemConfig) {
 		// TODO Auto-generated method stub
@@ -48,6 +59,11 @@ public class SystemConfigServiceImpl implements ISystemConfigService{
 		 }
 		return flag;
 	}
+/**
+ * 修改汽车系统配置信息的方法
+ *  @param systemConfig
+ * @return boolean 
+ */
 	@Override
 	public boolean updateSystemConfig(Systemconfig systemConfig) {
 		// TODO Auto-generated method stub
