4d577f7d6700bfc9243f1cd7fcb7be1dec519774
le15874811343 <576169235@qq.com>
2016-09-19 22:44:51 +0800

    品牌信息服务实现类

--- 后台/S4SSMManager/src/cn/com/service/impl/CarBrandServiceImpl.java
+++ 后台/S4SSMManager/src/cn/com/service/impl/CarBrandServiceImpl.java
@@ -10,12 +10,16 @@ import cn.com.pojo.*;
 
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 品牌信息服务实现类
+ * @author lej
+ */
 @Service
 public class CarBrandServiceImpl implements ICarBrandService,IPageDao {
 	@Resource
-  private ICarBrandDao brandDao= null;
+  private ICarBrandDao brandDao= null; //品牌信息操作接口的引用
 	@Resource(name="carBrandDaoImpl")
-  private IPageDao brandPage=null;
+  private IPageDao brandPage=null; //分页处理操作接口的引用
   
 	public ICarBrandDao getBrandDao() {
 	return brandDao;
@@ -32,19 +36,29 @@ public IPageDao getBrandPage() {
 public void setBrandPage(IPageDao brandPage) {
 	this.brandPage = brandPage;
 }
-
+  /**
+   * 按热度获取品牌信息的服务
+   * @return  Map<Integer, CarBrand>
+   */
 	@Override
 	public Map<Integer, Carbrand> getCarBrandByCount() {
 		// TODO Auto-generated method stub
 		return brandDao.getCarBrandByCount();
 	}
-	
+  /**
+   * 获取所有品牌信息的服务
+   * @return Map<Integer,CarBrand>
+   */
 	@Override
 	public Map<Integer, Carbrand> getAllBrand() {
 		// TODO Auto-generated method stub
 		return brandDao.getAllBrand();
 	}
-	
+  /**
+   * 根据品牌编号获取品牌信息的服务
+   * @param carBrand
+   * @return CarBrand
+   */
 	@Override
 	public Carbrand getBrandByID(Carbrand carBrand) {
 		// TODO Auto-generated method stub
@@ -65,19 +79,31 @@ public void setBrandPage(IPageDao brandPage) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+    /**
+     * 获取品牌信息的记录总条数的服务
+     * @return int
+     */	
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return brandPage.queryPersonCarCount(object);
 	}
-	
+/**
+ * 分页获取品牌信息的服务
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */		
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		return brandPage.showPersonCarList(curPage, rowsPrePage, object);
 	}
+ /**
+   * 添加品牌的服务
+   *@return boolean
+   */
 	@Override
 	public boolean addCarBrand(Carbrand carBrand) {
 		// TODO Auto-generated method stub
@@ -87,6 +113,10 @@ public void setBrandPage(IPageDao brandPage) {
 		}
 		return flag;
 	}
+ /**
+   * 修改品牌的服务
+   *@return boolean
+   */
 	@Override
 	public boolean updateCarBrand(Carbrand carBrand) {
 		// TODO Auto-generated method stub
@@ -96,6 +126,10 @@ public void setBrandPage(IPageDao brandPage) {
 		}
 		return flag;
 	}
+  /**
+   * 删除品牌的服务
+   *@return boolean
+   */
 	@Override
 	public boolean deleteCarBrand(Carbrand carBrand) {
 		// TODO Auto-generated method stub
@@ -105,7 +139,11 @@ public void setBrandPage(IPageDao brandPage) {
 		}
 		return flag;
 	}
-
+  /**
+   * 根据品牌名称获取品牌信息的服务
+   * @param carBrand
+   * @return CarBrand
+   */
 	@Override
 	public Carbrand getBrandByName(Carbrand carBrand) {
 		// TODO Auto-generated method stub
