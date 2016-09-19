71ec4306f0313880a8dd61e7cd44493ed88993b4
le15874811343 <576169235@qq.com>
2016-09-19 21:43:53 +0800

    车系信息操作接口

--- 后台/S4SSMManager/src/cn/com/dao/IModelDao.java
+++ 后台/S4SSMManager/src/cn/com/dao/IModelDao.java
@@ -2,9 +2,33 @@ package cn.com.dao;
 import java.util.*;
 
 import cn.com.pojo.*;
+/**
+ * 车系信息操作接口
+ * @author lej
+ */
 public interface IModelDao {
+       /**
+       * 按条件获取车系信息的方法
+       * @parma model
+       *@return List<Model> 
+       */
       public List<Model> getModelByWhere(Model model);
+         /**
+       * 添加车系信息的方法
+       * @parma model
+       *@return int
+       */
       public int addModel(Model model);
+       /**
+       * 修改车系信息的方法
+       * @parma model
+       *@return int
+       */
       public int updateModel(Model model);
+      /**
+       * 删除车系信息的方法
+       * @parma model
+       *@return int
+       */
       public int deleteModel(Model model);
 }
