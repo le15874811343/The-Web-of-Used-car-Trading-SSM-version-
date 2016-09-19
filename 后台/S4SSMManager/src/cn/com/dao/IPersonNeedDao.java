9c1aa7ebbb79d27e601acd155f1447078463259a
le15874811343 <576169235@qq.com>
2016-09-19 21:48:22 +0800

    个人需求信息操作接口

--- 后台/S4SSMManager/src/cn/com/dao/IPersonNeedDao.java
+++ 后台/S4SSMManager/src/cn/com/dao/IPersonNeedDao.java
@@ -1,12 +1,44 @@
 package cn.com.dao;
 
 import cn.com.pojo.*;
-
+/**
+ * 个人需求信息操作接口
+ *@author 
+ */
 public interface IPersonNeedDao {
+   /**
+   * 
+   * 添加个人需求信息的方法
+   *@return int 
+   */
 public int addPersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 删除个人需求信息的方法
+   *@return int 
+   */
 public int deletePersonNeed(Personneed personNeed);
+ /**
+   * 
+   * 获取个人需求信息的方法
+   *@return PersonNeed
+   */
 public Personneed getPerSonNeed(Personneed personNeed);
+ /**
+   * 
+   * 修改个人需求信息的方法
+   *@return int 
+   */
 public int updatePersonNeed(Personneed personNeed);
+/**
+ * 
+ * 检查是否还有与某用户编号关联的个人需求信息
+ * @return boolean
+ */
 public boolean chekUidPerson(Personneed personneed);
+/**
+ * 根据用户编号删除个人需求信息的方法
+ *@return int  
+ */
 public int deleteUidPerson(Personneed personneed);
 }
