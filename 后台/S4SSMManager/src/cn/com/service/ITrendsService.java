cf255910a7d64dc6d438e66120f5171abb2b86a4
le15874811343 <576169235@qq.com>
2016-09-19 22:22:46 +0800

    公司动态消息服务接口

--- 后台/S4SSMManager/src/cn/com/service/ITrendsService.java
+++ 后台/S4SSMManager/src/cn/com/service/ITrendsService.java
@@ -5,12 +5,36 @@ import org.springframework.stereotype.Repository;
 
 
 import cn.com.pojo.*;
+/**
+ * 公司动态消息服务接口
+ * @author
+ */
 @Repository
 public interface ITrendsService {
+	 /**
+        * 获取指定条数的最新的动态消息集合的的服务
+        * @param  rows 获取记录的条数
+        * @return List<Trends> 
+        */
 	public List<Trends> getITrendsByTime(Trends trends,int rows);
+	 /**
+        * 按条件获取动态消息的服务
+        * @return Trends 
+        */
 	public Trends getTrendsByWhere(Trends trends);
-	
+	 /**
+        * 添加动态消息的服务
+        * @return boolean 
+        */
 	public boolean addTrends(Trends trends);
+	 /**
+        * 删除动态消息的服务
+        * @return boolean 
+        */
 	public boolean deleteTrends(Trends trends);
+	 /**
+        * 修改动态消息的服务
+        * @return boolean 
+        */
 	public boolean updateTrends(Trends trends);
 }
