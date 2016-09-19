a3c5740aca20fea734338302cb212b4cc7b2e3ca
le15874811343 <576169235@qq.com>
2016-09-19 21:49:04 +0800

    营业额查询操作接口

--- 后台/S4SSMManager/src/cn/com/dao/IPriceDao.java
+++ 后台/S4SSMManager/src/cn/com/dao/IPriceDao.java
@@ -5,10 +5,30 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 营业额查询操作接口
+ * 
+ */
 @Repository
 public interface IPriceDao {
+	/**
+	 * 按年月日查询的方法
+	 * 
+	 */
 	public  List<Price> getsellinfo(int year,int month,int day);
+	/**
+	 * 按年月查询的方法
+	 * 
+	 */
 	public List<Price> getsellinfo(int year,int month);
+	/**
+	 * 按年查询的方法
+	 * 
+	 */
 	public List<Price> getsellinfo(int year);
+	/**
+	 * 查询不同年营业额的方法
+	 * 
+	 */
 	public List<Price> getsellinfo();
 }
