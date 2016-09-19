aafe70493133b83194cc33eb14e3fa31e60c87bb
le15874811343 <576169235@qq.com>
2016-09-19 22:59:06 +0800

    个人汽车订单信息服务实现类

--- 后台/S4SSMManager/src/cn/com/service/impl/PersonCarServiceImpl.java
+++ 后台/S4SSMManager/src/cn/com/service/impl/PersonCarServiceImpl.java
@@ -11,12 +11,16 @@ import cn.com.dao.IPageDao;
 import cn.com.dao.IPersonCarDao;
 
 import cn.com.service.*;
+/**
+ * 个人汽车订单信息服务实现类
+ * @author lej
+ */
 @Service
 public class PersonCarServiceImpl implements IPerSonCarService,IPageDao {
 	@Resource
-   private IPersonCarDao personCarDao=null;
+   private IPersonCarDao personCarDao=null; //个人汽车订单操作接口的引用
 	@Resource(name="personCarDaoImpl")
-   private IPageDao  personcarPage=null;
+   private IPageDao  personcarPage=null;  //分页处理操作接口的引用
    
 	public IPersonCarDao getPersonCarDao() {
 	return personCarDao;
@@ -30,11 +34,19 @@ public IPageDao getPersoncarPage() {
 public void setPersoncarPage(IPageDao personcarPage) {
 	this.personcarPage = personcarPage;
 }
+       /**
+        * 根据条件获取用户订单信息数量的服务
+        * @return int
+        */
 	@Override
 	public Long getCarCountByWhere(Personcar perSonCar) {
 		// TODO Auto-generated method stub
 		return personCarDao.getCarCountByWhere(perSonCar);
 	}
+	/**
+        * 根据条件获取用户订单信息集合的服务
+        * @return  Map<Long, PerSonCar>
+        */
 	@Override
 	public Map<Long, Personcar> getPerSonCarMapByWhere(Personcar perSonCar) {
 		// TODO Auto-generated method stub
@@ -53,17 +65,31 @@ public void setPersoncarPage(IPageDao personcarPage) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	 /**
+        * 根据条件获取用户订单信息数量的服务
+        * @return int
+        */
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return personcarPage.queryPersonCarCount(object);
 	}
+	/**
+	 * 根据条件分页获取用户订车详情集合的服务
+	 * @param curPage 当前页
+	 * @param rowsPrePage 每页面记录数
+	 * @return Map<Long,Object>
+	 */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		return personcarPage.showPersonCarList(curPage, rowsPrePage, object);
 	}
+	  /**
+        *  添加用户订单信息的服务
+        * @return boolean
+        */
 	@Override
 	public boolean addPerSonCar(Personcar perSonCar) {
 		// TODO Auto-generated method stub
@@ -73,6 +99,10 @@ public void setPersoncarPage(IPageDao personcarPage) {
 		}
 		return flag;
 	}
+	 /**
+        *  删除用户订单信息的服务
+        * @return boolean
+        */
 	@Override
 	public boolean deletePerSoncar(Personcar perSonCar) {
 		// TODO Auto-generated method stub
@@ -82,6 +112,10 @@ public void setPersoncarPage(IPageDao personcarPage) {
 		}
 		return flag;
 	}
+	 /**
+        *  修改用户订单信息的服务
+        * @return boolean
+        */
 	@Override
 	public boolean updatePerSoncar(Personcar perSonCar, String state) {
 		// TODO Auto-generated method stub
@@ -91,12 +125,22 @@ public void setPersoncarPage(IPageDao personcarPage) {
 		}
 		return flag;
 	}
+	 /**
+	 * 根据条件分页获取记录集合的服务
+	 * @param curPage 当前页
+	 * @param rowsPrePage 每页面记录数
+	 * @return Map<Long,PerSonCar>
+	 */
 	@Override
 	public Map<Long, Personcar> getPersonList(int curPage, int rowsPrePage,
 			Personcar perSonCar) {
 		// TODO Auto-generated method stub
 		return personCarDao.getPersonList(curPage, rowsPrePage, perSonCar);
 	}
+/**
+ * 根据用户编号删除用户订单信息的服务
+ *@return boolean  
+ */
 	@Override
 	public boolean deletepersoncaruser(Personcar p) {
 		// TODO Auto-generated method stub
@@ -108,6 +152,10 @@ public void setPersoncarPage(IPageDao personcarPage) {
 		
 		return flag;
 	}
+/**
+ * 根据车编号删除用户订单信息的服务
+ *@return boolean  
+ */
 	@Override
 	public boolean deletecidpersoncaruser(Personcar p) {
 		// TODO Auto-generated method stub
@@ -119,16 +167,30 @@ public void setPersoncarPage(IPageDao personcarPage) {
 		
 		return flag;
 	}
+/**
+ * 
+ * 检查是否还有与某用户编号关联的用户订单信息的服务
+ * @return boolean
+ */
 	@Override
 	public boolean checkipersoncaruser(Personcar p) {
 		// TODO Auto-generated method stub
 		return personCarDao.checkipersonuser(p);
 	}
+/**
+ * 
+ * 检查是否还有与某车编号关联的用户订单信息的服务
+ * @return boolean
+ */
 	@Override
 	public boolean checkcidipersoncaruser(Personcar p) {
 		// TODO Auto-generated method stub
 		return personCarDao.checkcidipersonuser(p);
 	}
+/**
+ * 根据所有字段删除用户订单信息的方法的服务
+ *@return boolean  
+ */
 	@Override
 	public boolean deletePersonByAll(Personcar perSonCar) {
 		// TODO Auto-generated method stub
@@ -140,16 +202,30 @@ public void setPersoncarPage(IPageDao personcarPage) {
 		
 		return flag;
 	}
+/**
+ * 
+ * 获取用户订单信息的服务
+ * @return Personcar
+ */
 	@Override
 	public Personcar getPersoncar(Personcar personcar) {
 		// TODO Auto-generated method stub
 		return personCarDao.getPersoncar(personcar);
 	}
+/**
+ * 
+ * 检查是否还有与某车主编号关联的用户订单信息的服务
+ * @return boolean
+ */
 	@Override
 	public boolean checkcuidperson(Personcar p) {
 		// TODO Auto-generated method stub
 		return personCarDao.checkcuidperson(p);
 	}
+ /**
+ * 根据车主编号删除用户订单信息的服务
+ *@return boolean  
+ */
 	@Override
 	public boolean deletecuidperson(Personcar p) {
 		// TODO Auto-generated method stub
