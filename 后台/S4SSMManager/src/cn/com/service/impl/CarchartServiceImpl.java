8d155fbb9db6a300f8d97a85c1af3f854456ad03
le15874811343 <576169235@qq.com>
2016-09-19 22:50:13 +0800

    品牌销量服务实现类

--- 后台/S4SSMManager/src/cn/com/service/impl/CarchartServiceImpl.java
+++ 后台/S4SSMManager/src/cn/com/service/impl/CarchartServiceImpl.java
@@ -11,10 +11,14 @@ import cn.com.pojo.Carchart;
 import cn.com.dao.ICarchartDao;
 
 import cn.com.service.ICarchartService;
+/**
+ * 品牌销量服务实现类
+ * 
+ */
 @Service
 public class CarchartServiceImpl implements ICarchartService{
 	@Resource
-private ICarchartDao carchartDao=null;
+private ICarchartDao carchartDao=null; //品牌销量操作接口的引用
 	
 	public ICarchartDao getCarchartDao() {
 		return carchartDao;
@@ -22,24 +26,40 @@ private ICarchartDao carchartDao=null;
 	public void setCarchartDao(ICarchartDao carchartDao) {
 		this.carchartDao = carchartDao;
 	}
+  /**
+   * 按年月查询的服务
+   * 
+   */
 	@Override
 	public List<Carchart> chartinfo(int year, int month) {
 		
 		// TODO Auto-generated method stub
 		return carchartDao.chartinfo(year, month);
 	}
+/**
+ * 查询不同年的服务
+ * 
+ */
 	@Override
 	public List<Carchart> chartinfo() {
 		// TODO Auto-generated method stub
 		
 		return carchartDao.chartinfo();
 	}
+/**
+ * 按年月日查询的服务
+ * 
+ */
 	@Override
 	public Map<String, Carchart> chartinfo(int year, int month, int day) {
 		// TODO Auto-generated method stub
 		
 		return carchartDao.chartinfo(year, month, day);
 	}
+/**
+ * 按年查询的服务
+ * 
+ */
 	@Override
 	public Map<String, Carchart> chartinfo(int year) {
 		// TODO Auto-generated method stub
