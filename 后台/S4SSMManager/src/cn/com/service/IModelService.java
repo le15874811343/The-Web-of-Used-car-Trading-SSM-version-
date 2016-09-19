00d7f51cefe827ef205f03f2a9d7cafc763131f4
le15874811343 <576169235@qq.com>
2016-09-19 22:13:42 +0800

    车系信息服务接口

--- 后台/S4SSMManager/src/cn/com/service/IModelService.java
+++ 后台/S4SSMManager/src/cn/com/service/IModelService.java
@@ -5,10 +5,34 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 
 import cn.com.pojo.*;
+/**
+ * 车系信息服务接口
+ * @author lej
+ */
 @Repository
 public interface IModelService {
+      /**
+       * 按条件获取车系信息的服务
+       * @parma model
+       *@return List<Model> 
+       */
 	public List<Model> getModelByWhere(Model model);
+      /**
+       * 添加车系信息的服务
+       * @parma model
+       *@return boolean
+       */
 	public boolean addModel(Model model);
+      /**
+       * 修改车系信息的服务
+       * @parma model
+       *@return boolean
+       */
 	public boolean updateModel(Model model);
+      /**
+       * 删除车系信息的服务
+       * @parma model
+       *@return boolean
+       */
 	public boolean deleteModel(Model model);
 }
