9ee4e15f56fc7ca394130e61fbc970cf9640fd80
le15874811343 <576169235@qq.com>
2016-09-19 22:40:25 +0800

    汽车基本信息服务实现类

--- 后台/S4SSMManager/src/cn/com/service/impl/BasicInfoServiceImpl.java
+++ 后台/S4SSMManager/src/cn/com/service/impl/BasicInfoServiceImpl.java
@@ -11,10 +11,14 @@ import cn.com.pojo.*;
 import cn.com.dao.IBasicInfoDao;
 
 import cn.com.service.*;
+/**
+ * 汽车基本信息服务实现类
+ * @author lej
+ */
 @Service
 public class BasicInfoServiceImpl implements IBasicInfoService{
 	@Resource
-private IBasicInfoDao basicInfoDaoImpl=null;
+private IBasicInfoDao basicInfoDaoImpl=null; //汽车基础信息操作接口的引用
 
 public IBasicInfoDao getBasicInfoDaoImpl() {
 	return basicInfoDaoImpl;
@@ -23,19 +27,28 @@ public IBasicInfoDao getBasicInfoDaoImpl() {
 public void setBasicInfoDaoImpl(IBasicInfoDao basicInfoDaoImpl) {
 	this.basicInfoDaoImpl = basicInfoDaoImpl;
 }
-
+/**
+ * 获取所有基本信息的服务
+ * @return Map<Long, BasicInfo>
+ */
 @Override
 public Map<Long, Basicinfo> getAllBasic() {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.getAllBasic();
 }
-
+/**
+ *根据编号获取汽车基本信息的服务
+ *@return BasicInfo
+ */
 @Override
 public Basicinfo getAllBasicById(Carinfo carInfo) {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.getAllBasicById(carInfo);
 }
-
+/**
+ * 添加汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean addBasicInfo(Basicinfo basicInfo) {
 	// TODO Auto-generated method stub
@@ -45,7 +58,10 @@ public boolean addBasicInfo(Basicinfo basicInfo) {
 	}
 	return flag;
 }
-
+/**
+ * 修改汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean updateBasicInfo(Basicinfo basicInfo) {
 	// TODO Auto-generated method stub
@@ -55,7 +71,10 @@ public boolean updateBasicInfo(Basicinfo basicInfo) {
 	}
 	return flag;
 }
-
+/**
+ * 根据车主编号删除汽车基本信息的服务
+ *@return boolean  
+ */
 @Override
 public boolean deletebasicinfo(Basicinfo b) {
 	// TODO Auto-generated method stub
@@ -67,7 +86,10 @@ public boolean deletebasicinfo(Basicinfo b) {
 	
 	return flag;
 }
-
+/**
+ * 根据车编号删除汽车基本信息的服务
+ *@return boolean  
+ */
 @Override
 public boolean deletecidbasicinfo(Basicinfo b) {
 	// TODO Auto-generated method stub
@@ -79,13 +101,19 @@ public boolean deletecidbasicinfo(Basicinfo b) {
 	
 	return flag;
 }
-
+/**
+ * 检查是否还有与某车主编号关联的汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean checkbasicinfo(Basicinfo b) {
 	// TODO Auto-generated method stub
 	return basicInfoDaoImpl.checkbasicinfo(b);
 }
-
+/**
+ * 检查是否还有与某车编号关联的汽车基本信息的服务
+ * @return boolean
+ */
 @Override
 public boolean checkcidbasicinfo(Basicinfo b) {
 	// TODO Auto-generated method stub
