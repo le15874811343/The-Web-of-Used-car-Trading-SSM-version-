e82d7b410f0c3dac29dafd82a6571c4020f3bea2
le15874811343 <576169235@qq.com>
2016-09-19 22:09:01 +0800

    品牌销量服务接口

--- 后台/S4SSMManager/src/cn/com/service/ICarchartService.java
+++ 后台/S4SSMManager/src/cn/com/service/ICarchartService.java
@@ -8,10 +8,30 @@ import org.springframework.stereotype.Repository;
 
 
 import cn.com.pojo.Carchart;
+/**
+ * 品牌销量服务接口
+ * 
+ */
 @Repository
 public interface ICarchartService {
+  /**
+   * 按年月查询的服务
+   * 
+   */
 	public List<Carchart> chartinfo(int year,int month);
+/**
+ * 查询不同年的服务
+ * 
+ */
 	public List<Carchart> chartinfo();
+/**
+ * 按年月日查询的服务
+ * 
+ */
 	public Map<String, Carchart> chartinfo(int year, int month, int day);
+/**
+ * 按年查询的服务
+ * 
+ */
 	public Map<String, Carchart> chartinfo(int year);
 }
