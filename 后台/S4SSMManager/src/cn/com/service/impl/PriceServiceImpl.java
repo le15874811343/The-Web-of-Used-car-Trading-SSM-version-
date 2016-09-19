6f5cce5afbbca39e2bc3dfe3c242162bf3ac1a3d
le15874811343 <576169235@qq.com>
2016-09-19 23:02:29 +0800

    营业额查询服务实现类

--- 后台/S4SSMManager/src/cn/com/service/impl/PriceServiceImpl.java
+++ 后台/S4SSMManager/src/cn/com/service/impl/PriceServiceImpl.java
@@ -10,10 +10,14 @@ import cn.com.pojo.Price;
 import cn.com.dao.IPriceDao;
 import cn.com.dao.impl.PriceDaoImpl;
 import cn.com.service.IPriceService;
+/**
+ * 营业额查询服务实现类
+ * 
+ */
 @Service
 public class PriceServiceImpl implements IPriceService {
     @Resource(name="priceDaoImpl")
-	private IPriceDao priceDao=null;
+	private IPriceDao priceDao=null; //营业额查询操作接口的引用
    
 
 	public IPriceDao getPriceDao() {
@@ -23,25 +27,37 @@ public class PriceServiceImpl implements IPriceService {
 	public void setPriceDao(IPriceDao priceDao) {
 		this.priceDao = priceDao;
 	}
-
+        /**
+	 * 按年月日查询的服务
+	 * 
+	 */
 	@Override
 	public  List<Price> getsellinfo(int year, int month, int day) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year, month, day);
 	}
-
+        /**
+ 	 * 按年月查询的服务
+	 * 
+	 */
 	@Override
 	public List<Price> getsellinfo(int year, int month) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year, month);
 	}
-
+        /**
+	 * 按年查询的服务
+	 * 
+	 */
 	@Override
 	public  List<Price> getsellinfo(int year) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year);
 	}
-
+        /**
+	 * 查询不同年营业额的服务
+	 * 
+	 */
 	@Override
 	public List<Price> getsellinfo() {
 		// TODO Auto-generated method stub
