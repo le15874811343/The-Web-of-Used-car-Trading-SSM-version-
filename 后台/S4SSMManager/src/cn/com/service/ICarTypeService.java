69a1dac748c70e0d3ba1bdce3305b26b61426078
le15874811343 <576169235@qq.com>
2016-09-19 22:08:10 +0800

    车型服务接口

--- 后台/S4SSMManager/src/cn/com/service/ICarTypeService.java
+++ 后台/S4SSMManager/src/cn/com/service/ICarTypeService.java
@@ -5,13 +5,40 @@ import cn.com.pojo.*;
 import java.util.*;
 
 import org.springframework.stereotype.Repository;
-
+/**
+ * 车型服务接口
+ * @author
+ */
 @Repository
 public interface ICarTypeService {
+	/**
+	 * 按热度获取车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 	public Map<Integer, Cartype> getCarTypeByCount();
+	/**
+	 * 获取所有车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 	public Map<Integer, Cartype> getAllCarType();
+	/**
+	 * 获取所有车型信息的服务
+	 * @return Map<Integer,CarType>
+	 */
 	public boolean addCarType(Cartype carType);
+/**
+ * 删除车型的服务
+ * @return boolean
+ */
 	public boolean deleteCarType(Cartype carType);
+/**
+ * 修改车型的服务
+ * @return boolean
+ */
 	public boolean updateType(Cartype carType);
+/**
+ * 获取车型的服务
+ * @return CarType
+ */
 	public Cartype getCarType(Cartype carType);
 }
