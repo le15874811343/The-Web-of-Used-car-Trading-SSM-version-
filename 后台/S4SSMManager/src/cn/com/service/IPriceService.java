8aee1c9248eaea5a74f00f996c818247c15f8d50
le15874811343 <576169235@qq.com>
2016-09-19 22:17:49 +0800

    营业额查询服务接口

--- 后台/S4SSMManager/src/cn/com/service/IPriceService.java
+++ 后台/S4SSMManager/src/cn/com/service/IPriceService.java
@@ -7,10 +7,30 @@ import org.springframework.stereotype.Repository;
 
 
 import cn.com.pojo.Price;
+/**
+ * 营业额查询服务接口
+ * 
+ */
 @Repository
 public interface IPriceService {
+	/**
+	 * 按年月日查询的服务
+	 * 
+	 */
 	public   List<Price> getsellinfo(int year,int month,int day);
+	/**
+	 * 按年月查询的服务
+	 * 
+	 */
 	public List<Price> getsellinfo(int year,int month);
+	/**
+	 * 按年查询的服务
+	 * 
+	 */
 	public  List<Price> getsellinfo(int year);
+	/**
+	 * 查询不同年营业额的服务
+	 * 
+	 */
 	public List<Price> getsellinfo();
 }
