eba39cc1ae6e2128cdd724194c13c495ebff63be
le15874811343 <576169235@qq.com>
2016-09-19 23:09:55 +0800

    公司动态消息服务实现类

--- 后台/S4SSMManager/src/cn/com/service/impl/TrendsServiceImpl.java
+++ 后台/S4SSMManager/src/cn/com/service/impl/TrendsServiceImpl.java
@@ -10,12 +10,16 @@ import cn.com.pojo.Trends;
 
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 公司动态消息服务实现类
+ * @author
+ */
 @Service
 public class TrendsServiceImpl implements ITrendsService,IPageDao{
 	@Resource
- private  ITrendsDao trendsDao=null;
+ private  ITrendsDao trendsDao=null;  //公司动态消息操作接口的引用
 	@Resource(name="trendsDaoImpl")
- private IPageDao  trendPage=null;
+ private IPageDao  trendPage=null;  //分页处理操作接口的引用
  
 	public IPageDao getTrendPage() {
 	return trendPage;
@@ -29,6 +33,11 @@ public void setTrendPage(IPageDao trendPage) {
 public void setTrendsDao(ITrendsDao trendsDao) {
 	this.trendsDao = trendsDao;
 }
+       /**
+        * 获取指定条数的最新的动态消息集合的的服务
+        * @param  rows 获取记录的条数
+        * @return List<Trends> 
+        */
 	@Override
 	public List<Trends> getITrendsByTime(Trends trends,int rows) {
 		// TODO Auto-generated method stub
@@ -47,22 +56,40 @@ public void setTrendsDao(ITrendsDao trendsDao) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+    /**
+     * 按条件获取公司动态信息的记录总条数
+     * @return int
+     */	
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return trendPage.queryPersonCarCount(object);
 	}
+/**
+ * 按条件分页获取公司动态消息信息
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */	
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		return trendPage.showPersonCarList(curPage, rowsPrePage, object);
 	}
+       /**
+        * 按条件获取动态消息的服务
+        * @return Trends 
+        */
 	@Override
 	public Trends getTrendsByWhere(Trends trends) {
 		// TODO Auto-generated method stub
 		return trendsDao.getTrendsByWhere(trends);
 	}
+	/**
+        * 添加动态消息的服务
+        * @return boolean 
+        */
 	@Override
 	public boolean addTrends(Trends trends) {
 		// TODO Auto-generated method stub
@@ -72,6 +99,10 @@ public void setTrendsDao(ITrendsDao trendsDao) {
 		}
 		return flag;
 	}
+	 /**
+        * 删除动态消息的服务
+        * @return boolean 
+        */
 	@Override
 	public boolean deleteTrends(Trends trends) {
 		// TODO Auto-generated method stub
@@ -81,6 +112,10 @@ public void setTrendsDao(ITrendsDao trendsDao) {
 		}
 		return flag;
 	}
+	/**
+        * 修改动态消息的服务
+        * @return boolean 
+        */
 	@Override
 	public boolean updateTrends(Trends trends) {
 		// TODO Auto-generated method stub
