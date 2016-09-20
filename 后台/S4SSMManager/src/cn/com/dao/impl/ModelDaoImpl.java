3cfbfdf4832f1ba827845afd59ee1ec4fed94b1d
le15874811343 <576169235@qq.com>
2016-09-20 22:34:16 +0800

    车系信息操作实现类

--- 后台/S4SSMManager/src/cn/com/dao/impl/ModelDaoImpl.java
+++ 后台/S4SSMManager/src/cn/com/dao/impl/ModelDaoImpl.java
@@ -5,9 +5,17 @@ import cn.com.dao.*;
 import java.util.*;
 
 import org.springframework.stereotype.Repository;
+/**
+ * 车系信息操作实现类
+ * @author lej
+ */
 @Repository
 public class ModelDaoImpl extends BaseDao implements IModelDao {
-
+      /**
+       * 按条件获取车系信息的方法
+       * @parma model
+       *@return List<Model> 
+       */
 	@Override
 	public List<Model> getModelByWhere(Model model) {
 		// TODO Auto-generated method stub
@@ -28,19 +36,31 @@ public class ModelDaoImpl extends BaseDao implements IModelDao {
 	}
 	return modelMap;
 	}
-
+      /**
+       * 添加车系信息的方法
+       * @parma model
+       *@return int
+       */
 	@Override
 	public int addModel(Model model) {
 		// TODO Auto-generated method stub
 		return super.getSqlSessionTemplate().insert("cn.com.pojo.ModelMapper.insertSelective", model);
 	}
-
+      /**
+       * 修改车系信息的方法
+       * @parma model
+       *@return int
+       */
 	@Override
 	public int updateModel(Model model) {
 		// TODO Auto-generated method stub
 		return super.getSqlSessionTemplate().update("cn.com.pojo.ModelMapper.updateByPrimaryKeySelective", model);
 	}
-
+      /**
+       * 删除车系信息的方法
+       * @parma model
+       *@return int
+       */
 	@Override
 	public int deleteModel(Model model) {
 		// TODO Auto-generated method stub
