09febb663aa23e558e0341168793e5f516126940
le15874811343 <576169235@qq.com>
2016-09-20 22:33:10 +0800

    汽车硬件配置信息操作实现类

--- 后台/S4SSMManager/src/cn/com/dao/impl/HardwareConfigDaoImpl.java
+++ 后台/S4SSMManager/src/cn/com/dao/impl/HardwareConfigDaoImpl.java
@@ -8,10 +8,17 @@ import org.springframework.stereotype.Repository;
 import cn.com.pojo.*;
 
 import cn.com.dao.IHardwareConfig;
-
+/**
+ * 汽车硬件配置信息操作实现类
+ *@author lej 
+ */
 @Repository
 public class HardwareConfigDaoImpl extends BaseDao implements IHardwareConfig {
-
+  /**
+   * 
+   * 获取所有汽车硬件配置信息的方法
+   *@return Map<Long,HardwareConfig> 
+   */
 	@Override
 	public Map<Long, Hardwareconfig> getAllHardwareConfig() {
 		// TODO Auto-generated method stub
@@ -20,7 +27,11 @@ public class HardwareConfigDaoImpl extends BaseDao implements IHardwareConfig {
 	hardMap=super.getSqlSessionTemplate().selectMap("cn.com.pojo.HardwareconfigMapper.getAllHardwareConfig","cId");
 		return hardMap;
 	}
-
+/**
+ * 按编号获取汽车硬件配置信息的方法
+ * @parma carInfo
+ * @return HardwareConfig 
+ */
 	@Override
 	public Hardwareconfig getHardwareConfigById(Carinfo carInfo) {
 		// TODO Auto-generated method stub
@@ -34,7 +45,11 @@ public class HardwareConfigDaoImpl extends BaseDao implements IHardwareConfig {
 		}
 		return hardwareConfig2;
 	}
-
+/**
+ * 添加汽车硬件配置信息的方法
+ * @parma hardwareConfig
+ * @return int
+ */
 	@Override
 	public int addHardwareConfig(Hardwareconfig hardwareConfig) {
 		// TODO Auto-generated method stub
@@ -49,7 +64,11 @@ public class HardwareConfigDaoImpl extends BaseDao implements IHardwareConfig {
 		}
 		return count;
 	}
-
+/**
+ * 修改汽车硬件配置信息的方法
+ * @parma hardwareConfig
+ * @return int
+ */
 	@Override
 	public int updateHardwareConfig(Hardwareconfig hardwareConfig) {
 		// TODO Auto-generated method stub
@@ -57,19 +76,29 @@ public class HardwareConfigDaoImpl extends BaseDao implements IHardwareConfig {
 		return  super.getSqlSessionTemplate().update("cn.com.pojo.HardwareconfigMapper.updateHardwareConfig", hardwareConfig);
 
 	}
-
+/**
+ * 根据车主编号删除汽车硬件配置信息的方法
+ *@return int  
+ */
 	@Override
 	public int deletehardwareconfiguser(Hardwareconfig h) {
 		// TODO Auto-generated method stub
 		return super.getSqlSessionTemplate().delete("cn.com.pojo.HardwareconfigMapper.deletehardwareconfiguser", h);
 	}
-
+/**
+ * 根据车编号删除汽车硬件配置信息的方法
+ *@return int  
+ */
 	@Override
 	public int deletecidhardwareconfiguser(Hardwareconfig h) {
 		// TODO Auto-generated method stub
 		return super.getSqlSessionTemplate().delete("cn.com.pojo.HardwareconfigMapper.deletecidhardwareconfiguser", h);
 	}
-
+/**
+ * 
+ * 检查是否还有与某车主编号关联的汽车硬件配置信息
+ * @return boolean
+ */
 	@Override
 	public boolean checkhardwareconfig(Hardwareconfig h) {
 		// TODO Auto-generated method stub
@@ -79,7 +108,11 @@ public class HardwareConfigDaoImpl extends BaseDao implements IHardwareConfig {
 		   }
 		return flag;
 	}
-
+/**
+ * 
+ * 检查是否还有与某车编号关联的汽车硬件配置信息
+ * @return boolean
+ */
 	@Override
 	public boolean checkcidhardwareconfig(Hardwareconfig h) {
 		// TODO Auto-generated method stub
