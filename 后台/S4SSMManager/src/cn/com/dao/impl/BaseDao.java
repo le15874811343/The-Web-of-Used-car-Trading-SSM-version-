8e2a3c0d0e9e2a1dcc89e43333fbf5c25149ea56
le15874811343 <576169235@qq.com>
2016-09-20 21:44:50 +0800

    持久化模板基类

--- 后台/S4SSMManager/src/cn/com/dao/impl/BaseDao.java
+++ 后台/S4SSMManager/src/cn/com/dao/impl/BaseDao.java
@@ -5,7 +5,9 @@ import javax.annotation.Resource;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Component;
 
-
+/**
+ * 持久化模板基类
+ */
 @Component
 public class BaseDao {
   @Resource
